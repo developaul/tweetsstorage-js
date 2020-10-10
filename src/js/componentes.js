42416f231975e8fec0ad46ed02fa1ccb3fbdb296
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-10 10:01:31 -0500

    addTeet agregado, agrega un nuevo tweet al array de tweets

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,2 +1,37 @@
 import '../css/normalize.css';
 import '../css/skeleton.css';
+
+// References
+const form = document.querySelector( '#formulario' );
+
+let tweets = [];
+
+
+// Functions
+// Agrega un nuevo tweet al array de tweets
+const addTweet = ( event ) => {
+    event.preventDefault();
+
+    const tweet = form.querySelector( '#tweet' ).value;
+
+    if( tweet === '' ) {
+        console.log( 'Un mensaje no puede ir vacío' );
+        return;
+    }
+
+    const tweetObj = { id: Date.now(), tweet }
+
+    tweets = [ ...tweets, tweetObj ];
+
+    // crearHTML();
+
+    form.reset();
+}
+
+
+
+
+// Events
+export const startEventListeners = () => {
+    form.addEventListener( 'submit', addTweet );
+}
